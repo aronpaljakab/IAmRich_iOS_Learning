82f8c53fddf1af5df47a2aa3f5d5b99c6b438ed1
Aron Pal-Jakab <aronpj.99@gmail.com>
2021-01-20 21:28:44 +0100

    Added working iOS app.

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Áron Pál-Jakab on 2019. January. 31.
+//  Copyright © 2019 Aron Pal-Jakab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
